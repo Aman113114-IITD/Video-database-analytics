db3814fd7c131b6c935d7a335ccde501e17898c7
Aman Bansal <amanbansalshram@gmail.com>
2023-08-29 12:17:36 +0530

    adding formatted code

--- setup/query_with_tracker.py
+++ setup/query_with_tracker.py
@@ -27,7 +27,6 @@ class YOLO:
         elif device is None:
             device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.model = torch.hub.load("ultralytics/yolov5", model_name, device=device)
-
     def __call__(
         self,
         img: Union[str, np.ndarray],
@@ -37,7 +36,6 @@ class YOLO:
         image_width: int = 1080,
         classes: Optional[List[int]] = None,
     ) -> torch.tensor:
-
         self.model.conf = conf_threshold
         self.model.iou = iou_threshold
         if classes is not None:
@@ -45,19 +43,14 @@ class YOLO:
         detections = self.model(img, size=(image_height,image_width))
         return detections
 
-
-
 # functions for tarcking objects in video
 def center(points):
     return [np.mean(np.array(points), axis=0)]
-
-# functions for tarcking objects in video
 def yolo_detections_to_norfair_detections(
     yolo_detections: torch.tensor, track_points: str = "centroid"  # bbox or centroid
 ) -> List[Detection]:
     """convert detections_as_xywh to norfair detections"""
     norfair_detections: List[Detection] = []
-
     if track_points == "centroid":
         detections_as_xywh = yolo_detections.xywh[0]
         for detection_as_xywh in detections_as_xywh:
@@ -89,61 +82,20 @@ def yolo_detections_to_norfair_detections(
                     points=bbox, scores=scores, label=int(detection_as_xyxy[-1].item())
                 )
             )
-
     return norfair_detections
 
-
-##parser arguements
-
-parser = argparse.ArgumentParser(description="Track objects in a video.")
-parser.add_argument("files", type=str, nargs="+", help="Video files to process")
-parser.add_argument(
-    "--model-name", type=str, default="yolov5x", help="YOLOv5 model name"
-)
-parser.add_argument(
-    "--img-height", type=int, default="1080", help="YOLOv5 inference height (pixels)"
-)
-parser.add_argument(
-    "--img-width", type=int, default="1080", help="YOLOv5 inference width (pixels)"
-)
-parser.add_argument(
-    "--conf-threshold",
-    type=float,
-    default="0.25",
-    help="YOLOv5 object confidence threshold",
-)
-parser.add_argument(
-    "--iou-threshold", type=float, default="0.45", help="YOLOv5 IOU threshold for NMS"
-)
-parser.add_argument(
-    "--classes",
-    nargs="+",
-    type=int,
-    help="Filter by class: --classes 0, or --classes 0 2 3",
-)
-parser.add_argument(
-    "--device", type=str, default=None, help="Inference device: 'cpu' or 'cuda'"
-)
-parser.add_argument(
-    "--track-points",
-    type=str,
-    default="centroid",
-    help="Track points: 'centroid' or 'bbox'",
-)
-args = parser.parse_args()
-
-
-
 # Function to answer query for finding truck in a given video
-def isObject(start,end,chunk_size,step_size,object,argum) :
-    
-    # detecting the model
+def query(start,end,chunk_size,step_size,object,argum) : 
+
+    # detecting the model to be used
     model = YOLO(args.model_name, device=args.device)
+
     # final answer
     ans=[]
 
-
     for input_video_path in argum.files:
+
+        # Initialize tracker
         distance_function = "iou" if argum.track_points == "bbox" else "euclidean"
         distance_threshold = (
             DISTANCE_THRESHOLD_BBOX
@@ -161,39 +113,37 @@ def isObject(start,end,chunk_size,step_size,object,argum) :
         if not cap.isOpened():
             print("Error opening video file.")
             return
-
-        # indices range to be processed based on starting and ending time passed in function
+        
+        # Indices range to be processed based on starting and ending time passed in function
         start_frame = fps*start
         end_frame = fps*end
 
         # Array to store output per frame with confidence score
-        output= deque()
-
-        # process video
-        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-
+        output= set()
         frame_count = 0
         valid_frames = 0
 
+        # Process video from start frame
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
         prev_hist = None
-
         while cap.isOpened() and start_frame < end_frame:
 
+            # Read next frame
             start_frame+=1
-
+            frame_count += 1
             ret, frame = cap.read()
             if not ret:
                 break
 
-            # Calculate the histogram of the current frame
+            # Calculate the histogram of the current frame and Normalize it
             hist = cv2.calcHist([frame], [0], None, [256], [0, 256])
-            hist = hist / hist.sum()  # Normalize the histogram
+            hist = hist / hist.sum()
 
-            objects=[]
-
+            # Process current frame
             if prev_hist is None or cv2.compareHist(hist, prev_hist, cv2.HISTCMP_INTERSECT) < HISTOGRAM_THRESHOLD:
-                #the frame is being taken
-                print("The frame is taken !!")
+                print("The frame is taken !! ",start_frame)
+                prev_hist = hist
+
                 # Perform object detection on the frame
                 results = model(
                     frame,
@@ -204,54 +154,48 @@ def isObject(start,end,chunk_size,step_size,object,argum) :
                     classes=argum.classes,
                 )
 
-                # do object tracking
+                # Do object tracking
                 detections = yolo_detections_to_norfair_detections(
                     results, track_points=argum.track_points
                 )
-                # print(detections)
                 tracked_objects = tracker.update(detections=detections)
-
+                if (len(tracked_objects)>0) :
+                    valid_frames+=1
                 for obj in tracked_objects:
                     if (obj.label==object) :
-                        objects.append(obj.id)
-
-                prev_hist = hist
-                                
+                        output.add(obj.id)          
             else:
-                print("The frame is not taken !!")
+                print("The frame is not taken !! ",start_frame)
 
-
-            output.append(objects)
-
-            frame_count += 1
-
-            if (len(objects)>0) :
-                valid_frames+=1
-
+            # If complete window is processed store results and reset for next window
             if (frame_count == chunk_size*fps) :
-                if (valid_frames>0) :
-                    do=set()
-                    for olist in output :
-                        for obj in olist :
-                            do.add(obj)
-                    ans.append((start_frame/fps-chunk_size,start_frame/fps,len(do),valid_frames))
-                    do.clear()
-                
-                for i in range(step_size*fps) :
-                    frame_count-=1
-                    if (len(output.popleft())>0) :
-                        valid_frames-=1
-                
-
+                ans.append((start_frame/fps-chunk_size,start_frame/fps,len(output),valid_frames))
+                output.clear()
+                frame_count=0
+                valid_frames=0
+                tracker = Tracker(
+                    distance_function=distance_function,
+                    distance_threshold=distance_threshold,
+                )
         cap.release()
-
     return ans
-        
+
+##parser arguements
+parser = argparse.ArgumentParser(description="Track objects in a video.")
+parser.add_argument("files", type=str, nargs="+", help="Video files to process")
+parser.add_argument("--model-name", type=str, default="yolov5x", help="YOLOv5 model name")
+parser.add_argument("--img-height", type=int, default="1080", help="YOLOv5 inference height (pixels)")
+parser.add_argument("--img-width", type=int, default="1080", help="YOLOv5 inference width (pixels)")
+parser.add_argument("--conf-threshold",type=float,default="0.25",help="YOLOv5 object confidence threshold",)
+parser.add_argument("--iou-threshold", type=float, default="0.45", help="YOLOv5 IOU threshold for NMS")
+parser.add_argument("--classes",nargs="+",type=int,help="Filter by class: --classes 0, or --classes 0 2 3",)
+parser.add_argument("--device", type=str, default=None, help="Inference device: 'cpu' or 'cuda'")
+parser.add_argument("--track-points",type=str,default="centroid",help="Track points: 'centroid' or 'bbox'",)
+args = parser.parse_args()
+
 # start time of program
 start_time = time.time()
-
-print(isObject(0,30,5,1,2,args))
-
+print(query(0,30,5,5,2,args))
 end_time = time.time()
 elapsed_time = end_time - start_time
 print("Elapsed time:", elapsed_time)

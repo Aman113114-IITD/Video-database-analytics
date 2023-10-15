612d8eb24b4a09380d65d109f9449d562db9c58a
Aman Bansal <amanbansalshram@gmail.com>
2023-10-15 19:29:59 +0530

    adding event class

--- BTP/src/main/java/btp/Event.java
+++ BTP/src/main/java/btp/Event.java
@@ -20,38 +20,63 @@ package btp;
 
 import java.util.Objects;
 
-public class Event{
+public class Event<A, B, C, D, E, F, G, H> {
+    private final A frame_id;
+    private final B obj_id;
+    private final C obj_class;
+    private final D color;
+    private final E xmin;
+    private final F ymin;
+    private final G xmax;
+    private final H ymax;
 
-    private String name;
-    private int id;
-
-    public Event(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public Event(A frame_id, B obj_id, C obj_class, D color, E xmin, F ymin, G xmax, H ymax) {
+        this.frame_id = frame_id;
+        this.obj_id = obj_id;
+        this.obj_class = obj_class;
+        this.color = color;
+        this.xmin = xmin;
+        this.ymin = ymin;
+        this.xmax = xmax;
+        this.ymax = ymax;
     }
 
-    public String getName() {
-        return name;
+    public A getframe_id() {
+        return frame_id;
+    }
+
+    public B getobj_id() {
+        return obj_id;
+    }
+
+    public C getobj_class() {
+        return obj_class;
+    }
+
+    public D getcolor() {
+        return color;
+    }
+
+    public E getxmin() {
+        return xmin;
+    }
+
+    public F getymin() {
+        return ymin;
+    }
+
+    public G getxmax() {
+        return xmax;
+    }
+
+    public H getymax() {
+        return ymax;
     }
 
     @Override
     public String toString() {
-        return ("Event(" + id + ", " + name + ")");
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Event) {
-            Event other = (Event) obj;
-
-            return name.equals(other.name) && id == other.id;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, id);
+        return "(" + frame_id + ", " + obj_id + ", " + obj_class + ", " +
+               color + ", " + xmin + ", " + ymin + ", " + xmax + ", " + ymax + ")";
     }
 }
+
--- BTP/src/main/java/btp/VideoAnalysis.java
+++ BTP/src/main/java/btp/VideoAnalysis.java
@@ -20,6 +20,7 @@ import java.util.Properties;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 
 public class VideoAnalysis {
 
@@ -37,13 +38,32 @@ public class VideoAnalysis {
             .setValueOnlyDeserializer(new SimpleStringSchema())
             .build();
         DataStream<String> kafkaStream = env.fromSource(kafkaSource,WatermarkStrategy.noWatermarks(), "Kafka Source");
-        Pattern<String, ?> pattern = Pattern.<String>begin("start")
-                .where(SimpleCondition.of(value -> true));
-        DataStream<Map<String,List<String>>> resultStream = CEP.pattern(kafkaStream, pattern).inProcessingTime()
-        .select(new PatternSelectFunction<String, Map<String,List<String>>>() {
+
+        String regex = "\\((\\d+), (\\d+), (\\d+), '(\\w+)', ([\\d.]+), ([\\d.]+), ([\\d.]+), ([\\d.]+)\\)";
+        DataStream<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> eventStream = kafkaStream.map(new MapFunction<String, Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>() {
             @Override
-            public Map<String,List<String>> select(Map<String, List<String>> pattern) throws Exception {
-                Map<String, List<String>> resultList = pattern;
+            public Event<Integer, Integer, Integer, String, Float, Float, Float, Float> map(String value) {
+                java.util.regex.Pattern patternf = java.util.regex.Pattern.compile(regex);
+                Matcher matcher = patternf.matcher(value);
+                int frame_id = Integer.parseInt(matcher.group(1));
+                int obj_id = Integer.parseInt(matcher.group(2));
+                int obj_class = Integer.parseInt(matcher.group(3));
+                String color = matcher.group(4);
+                float xmin = Float.parseFloat(matcher.group(5));
+                float ymin = Float.parseFloat(matcher.group(6));
+                float xmax = Float.parseFloat(matcher.group(7));
+                float ymax = Float.parseFloat(matcher.group(8));
+                return new Event<>(frame_id, obj_id, obj_class, color, xmin, ymin, xmax, ymax);
+            }
+        });
+
+        Pattern<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>, ?> pattern = Pattern.<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>begin("start")
+                .where(SimpleCondition.of(value -> true));
+        DataStream<Map<String,List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>>> resultStream = CEP.pattern(eventStream, pattern).inProcessingTime()
+        .select(new PatternSelectFunction<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>, Map<String,List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>>>() {
+            @Override
+            public Map<String,List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> select(Map<String, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> pattern) throws Exception {
+                Map<String, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> resultList = pattern;
                 return resultList;
             }
         });

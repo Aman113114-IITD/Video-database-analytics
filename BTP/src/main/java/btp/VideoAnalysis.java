058684dfdf5d7d888b466c7c11e40b56aac167f4
Aman Bansal <amanbansalshram@gmail.com>
2023-10-17 15:20:04 +0530

    adding cep sample

--- BTP/src/main/java/btp/VideoAnalysis.java
+++ BTP/src/main/java/btp/VideoAnalysis.java
@@ -21,53 +21,83 @@ import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.api.java.functions.KeySelector;
 
 public class VideoAnalysis {
 
-    public static void main(String[] args) throws Exception {
-        
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        Properties kafkaProps = new Properties();
-        kafkaProps.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        kafkaProps.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "flink-group");
-        // FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>("events", new SimpleStringSchema(), properties);
-        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
-            .setBootstrapServers("localhost:9092")
-            .setTopics("events")
-            .setStartingOffsets(OffsetsInitializer.latest())
-            .setValueOnlyDeserializer(new SimpleStringSchema())
-            .build();
-        DataStream<String> kafkaStream = env.fromSource(kafkaSource,WatermarkStrategy.noWatermarks(), "Kafka Source");
+	public static void main(String[] args) throws Exception {
+   	 
+    	StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    	Properties kafkaProps = new Properties();
+    	kafkaProps.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    	kafkaProps.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "flink-group");
+    	// FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>("events", new SimpleStringSchema(), properties);
+    	KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+        	.setBootstrapServers("localhost:9092")
+        	.setTopics("events")
+        	.setStartingOffsets(OffsetsInitializer.latest())
+        	.setValueOnlyDeserializer(new SimpleStringSchema())
+        	.build();
+    	DataStream<String> kafkaStream = env.fromSource(kafkaSource,WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-        String regex = "\\((\\d+), (\\d+), (\\d+), '(\\w+)', ([\\d.]+), ([\\d.]+), ([\\d.]+), ([\\d.]+)\\)";
-        DataStream<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> eventStream = kafkaStream.map(new MapFunction<String, Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>() {
-            @Override
-            public Event<Integer, Integer, Integer, String, Float, Float, Float, Float> map(String value) {
-                value = value.substring(1, value.length() - 1);
-                String[] values = value.split(", ");
-                int frame_id = Integer.parseInt(values[0]);
-                int obj_id = Integer.parseInt(values[1]);
-                int obj_class = Integer.parseInt(values[2]);
-                String color = values[3];
-                float xmin = Float.parseFloat(values[4]);
-                float ymin = Float.parseFloat(values[5]);
-                float xmax = Float.parseFloat(values[6]);
-                float ymax = Float.parseFloat(values[7]);
-                return new Event<>(frame_id, obj_id, obj_class, color, xmin, ymin, xmax, ymax);
-            }
-        });
+    	String regex = "\\((\\d+), (\\d+), (\\d+), '(\\w+)', ([\\d.]+), ([\\d.]+), ([\\d.]+), ([\\d.]+)\\)";
+    	DataStream<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> eventStream = kafkaStream.map(new MapFunction<String, Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>() {
+        	@Override
+        	public Event<Integer, Integer, Integer, String, Float, Float, Float, Float> map(String value) {
+            	value = value.substring(1, value.length() - 1);
+            	String[] values = value.split(", ");
+            	int frame_id = Integer.parseInt(values[0]);
+            	int obj_id = Integer.parseInt(values[1]);
+            	int obj_class = Integer.parseInt(values[2]);
+            	String color = values[3];
+            	float xmin = Float.parseFloat(values[4]);
+            	float ymin = Float.parseFloat(values[5]);
+            	float xmax = Float.parseFloat(values[6]);
+            	float ymax = Float.parseFloat(values[7]);
+            	return new Event<>(frame_id, obj_id, obj_class, color, xmin, ymin, xmax, ymax);
+        	}
+    	});
 
-        Pattern<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>, ?> pattern = Pattern.<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>begin("start")
-                .where(SimpleCondition.of(value -> value.getobj_class()==5));
-        DataStream<Map<String,List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>>> resultStream = CEP.pattern(eventStream, pattern).inProcessingTime()
-        .select(new PatternSelectFunction<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>, Map<String,List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>>>() {
-            @Override
-            public Map<String,List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> select(Map<String, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> pattern) throws Exception {
-                Map<String, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> resultList = pattern;
-                return resultList;
-            }
-        });
-        resultStream.print();
-        env.execute();
-    }
+   	 Pattern<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>, ?> pattern = Pattern.<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>begin("redObject")
+   	 .where(
+   		 SimpleCondition.of(event -> true)
+   	 )
+   	 .next("blueObject")
+   	 .where(
+   		 new IterativeCondition<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>() {
+   			 @Override
+   			 public boolean filter(
+   				 Event<Integer, Integer, Integer, String, Float, Float, Float, Float> blueEvent,
+   				 Context<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> context
+   			 ) throws Exception {
+   				 // Check if the ymin of blueObject is less than ymin of redObject
+   				 Event<Integer, Integer, Integer, String, Float, Float, Float, Float> redEvent = context.getEventsForPattern("redObject").iterator().next();
+   				 return blueEvent.getymin() < redEvent.getymin();
+   			 }
+   		 }
+   	 );
+
+    	// Apply the pattern to the event stream
+   	 PatternStream<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> patternStream = CEP.pattern(eventStream, pattern).inProcessingTime();
+
+   	 // Select and print the matched patterns
+   	 DataStream<Tuple2<List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>>> resultStream = patternStream.select(
+   		 new PatternSelectFunction<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>, Tuple2<List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>>>() {
+   			 @Override
+   			 public Tuple2<List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> select(Map<String, List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>>> pattern) throws Exception {
+   				 List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> redObject = pattern.get("redObject");
+   				 List<Event<Integer, Integer, Integer, String, Float, Float, Float, Float>> blueObject = pattern.get("blueObject");
+   				 return new Tuple2<>(redObject, blueObject);
+   			 }
+   		 }
+   	 );
+    	resultStream.print();
+    	env.execute();
+	}
 }
+
+
+
+
